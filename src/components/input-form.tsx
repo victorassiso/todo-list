801a926fbab02d9f3a077a1840b0aebab5751965
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-27 01:11:31 -0300

    PATCH: Input focus outline and ring color

--- src/components/input-form.tsx
+++ src/components/input-form.tsx
@@ -22,7 +22,7 @@ export function InputForm({ addTask }: InputFormProps) {
     <form className="flex flex-row gap-2 text-gray-100" onSubmit={handleSubmit}>
       <input
         type="text"
-        className="flex-grow rounded-lg bg-gray-500 p-4 placeholder-gray-300"
+        className="flex-grow rounded-lg bg-gray-500 p-4 placeholder-gray-300 focus:border-purple-dark focus:outline-none focus:ring-1 focus:ring-purple-dark"
         placeholder="Adicione uma nova tarefa"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
@@ -30,7 +30,7 @@ export function InputForm({ addTask }: InputFormProps) {
       <button
         type="submit"
         onClick={handleSubmit}
-        className="justify-content flex items-center gap-2 rounded-lg bg-blue-dark p-4"
+        className="justify-content flex items-center gap-2 rounded-lg bg-blue-dark p-4 hover:bg-blue"
       >
         <span className="text-sm font-bold text-gray-100">Criar</span>
         <img src={addIcon} alt="Plus icon" />
--- src/components/task.tsx
+++ src/components/task.tsx
@@ -9,7 +9,7 @@ export interface TaskProps {
 
 export function Task({ name, status }: TaskProps) {
   return (
-    <div className="border-1 mt-3 flex flex-row items-start gap-3 rounded-lg border-gray-400 bg-gray-500 p-4 shadow-[0_2px_8px_0_rgba(0,0,0,0.06)]">
+    <div className="border-1 focus:border-teal mt-3 flex flex-row items-start gap-3 rounded-lg border-gray-400 bg-gray-500 p-4 shadow-[0_2px_8px_0_rgba(0,0,0,0.06)] hover:border-gray-400 hover:bg-gray-400">
       <div>
         {status ? (
           <img className="p-1" src={checkboxTrue} alt="" />
