307c751734ea84f36751e37b0e777e4a22bb892a
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-27 00:44:30 -0300

    FEAT: Implement Add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
 import './global.css'
 
+import { useState } from 'react'
+
 import { Header } from './components/header'
 import { InputForm } from './components/input-form'
+import { TaskProps } from './components/task'
 import { TaskList } from './components/task-list'
 
-const tasks = [
-  {
-    name: 'Task 1',
-    status: true,
-  },
-  {
-    name: 'Task 2',
-    status: false,
-  },
-]
-
 export function App() {
+  const [taskList, setTaskList] = useState<TaskProps[]>([])
+
+  const addTask = (newTask: TaskProps) => {
+    setTaskList((taskList) => [...taskList, newTask])
+  }
   return (
     <div>
       <Header />
-      {/* Content */}
       <div className="-my-7 mx-auto mb-7 w-[736px] flex-grow flex-col">
-        <InputForm />
-        <TaskList taskList={tasks} />
+        <InputForm addTask={addTask} />
+        <TaskList taskList={taskList} />
       </div>
     </div>
   )
--- src/components/input-form.tsx
+++ src/components/input-form.tsx
@@ -1,12 +1,28 @@
+import React, { useState } from 'react'
+
 import addIcon from '../../public/images/plus.svg'
 
-export function InputForm() {
+interface InputFormProps {
+  addTask: (newTask: { name: string; status: boolean }) => void
+}
+
+export function InputForm({ addTask }: InputFormProps) {
+  const [taskName, setTaskName] = useState('')
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (taskName.trim() !== '') {
+      addTask({ name: taskName, status: false })
+      setTaskName('')
+    }
+  }
   return (
-    <form className="flex flex-row gap-2">
+    <form className="flex flex-row gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         className="flex-grow rounded-lg bg-gray-500 p-4 placeholder-gray-300"
         placeholder="Adicione uma nova tarefa"
+        onChange={(e) => setTaskName(e.target.value)}
       ></input>
       <button
         type="submit"
